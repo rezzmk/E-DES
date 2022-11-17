d3ad6f5c84c8c4fa335402321ec6229ffdb69923
Marcos Caramalho <marcos.caramalho@gmail.com>
2022-11-17 11:05:13 +0000

    adds stdin support

--- encrypt.c
+++ encrypt.c
@@ -9,15 +9,19 @@
 
 #define MAX_BUF_SIZE 4096
 
+void print_buf(uint8_t *buf, size_t buf_len);
+void print_help();
+uint8_t *try_read_from_stdin(uint64_t *size);
+
 int main(int argc, char **args) {
    int option;
 
-   uint8_t *key_str;
-   uint8_t *file_name;
-   uint8_t *output_file;
+   uint8_t *key_str = NULL;
+   uint8_t *file_name = NULL;
+   uint8_t *output_file = NULL;
    bool legacy = false;
 
-   while ((option = getopt(argc, args, ":k:f:m:o:l")) != -1) {
+   while ((option = getopt(argc, args, ":k:f:m:o:l:h")) != -1) {
       switch (option) {
       case 'f':
          file_name = optarg;
@@ -32,6 +36,9 @@ int main(int argc, char **args) {
          printf("using legacy DES\n");
          legacy = true;
          break;
+      case 'h':
+         print_help();
+         return 0;
       case ':':
          printf("option needs a value\n");
          break;
@@ -40,21 +47,26 @@ int main(int argc, char **args) {
          break;
       }
    }
+
    bool ok = true;
    if (key_str == NULL) {
-      printf("The following options are missing: \n-k (key)\n");
+      printf("\nThe following options are missing: \n-k (key)\n");
       ok = false;
    }
-
-   if (file_name == NULL) {
-      printf("-f (input file name)\n");
-      ok = false;
-   }
-   if (!ok) return 1;
-
 
    uint64_t file_length = 0;
-   uint8_t *buffer = read_file_bytes(file_name, &file_length);
+   uint8_t *stdin_input = try_read_from_stdin(&file_length);
+   if (file_name == NULL && stdin_input == NULL) {
+      printf("-f (input file name). You can also use input redirection with '<'\n");
+      ok = false;
+   }
+   if (!ok) {
+      printf("\n\n");
+      print_help();
+      return 1;
+   }
+
+   uint8_t *buffer = stdin_input != NULL ? stdin_input : read_file_bytes(file_name, &file_length);
    uint8_t *key = get_sha_256(key_str);
 
    Algorithm algo = EDES;
@@ -69,8 +81,55 @@ int main(int argc, char **args) {
 
    if (output_file != NULL) {
       write_file_bytes(encryption_result->result, encryption_result->length, output_file);
+   } else {
+      if (encryption_result->length > 256) {
+         printf("Printing first 256 bytes of result, saving everything to c-enc\n\n\tCiphertext (in bytes): ");
+         write_file_bytes(encryption_result->result, encryption_result->length, "c-enc");
+         print_buf(encryption_result->result, 256);
+      }
+      else {
+         print_buf(encryption_result->result, encryption_result->length);
+      }
    }
 
    CAENC_CTX_cleanup();
    return 0;
+}
+
+void print_buf(uint8_t *buf, size_t buf_len) {
+   for (int i = 0; i < buf_len; i++) {
+      printf("%x ", buf[i]);
+   }
+   printf("\n");
+}
+
+void print_help() {
+   printf("Options:\n");
+   printf("\t-f {input file name}\n");
+   printf("\t-o {output file name}\n");
+   printf("\t-k {key string}\n");
+   printf("\t-l (if set, DES will be used, instead of E-DES)\n");
+}
+
+uint8_t *try_read_from_stdin(uint64_t *size) {
+   if (!isatty(STDIN_FILENO)) {
+      uint8_t *buffer = 0;
+      uint64_t n = 0;
+      uint32_t current_ch; // EOF represented by more than 1 char
+
+      do {
+         buffer = (uint8_t*) realloc(buffer, n + 1);
+         current_ch = fgetc(stdin);
+         buffer[n] = current_ch;
+         if (current_ch != EOF) {
+            n++;
+         }
+      }
+      while (current_ch != EOF);
+
+      *size = n;
+      return buffer;
+   }
+
+   return NULL;
 }
